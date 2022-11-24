1d44cc9a7f2cf56efe4932edda305178b61f9b30
konkos <kon_kostas@windowslive.com>
2022-11-24 02:13:47 +0200

    Comment on RegistrationController

--- src/main/java/com/api/springstudentsapi/controllers/RegistrationController.java
+++ src/main/java/com/api/springstudentsapi/controllers/RegistrationController.java
@@ -1,15 +1,15 @@
 package com.api.springstudentsapi.controllers;
 
 import com.api.springstudentsapi.entities.Course;
-import com.api.springstudentsapi.entities.Registration;
 import com.api.springstudentsapi.entities.Student;
 import com.api.springstudentsapi.services.CourseService;
 import com.api.springstudentsapi.services.RegistrationService;
 import com.api.springstudentsapi.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Collection;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/registration")
@@ -25,12 +25,14 @@ public class RegistrationController {
         this.courseService = courseService;
     }
 
+    //Should be PostMapping because a resource (Registration) is being created
     @PutMapping(path = "register")
-    public void registerStudentToCourse(
-            @RequestParam(name = "student") Long studentId,
-            @RequestParam(name = "course") Long courseId){
+    public void registerStudentToCourse(@RequestParam(name = "student") Long studentId, @RequestParam(name = "course") Long courseId) {
+        // This belongs in the service layer
         Course foundCourse = courseService.getCourseById(courseId);
         Student foundStudent = studentService.getStudentById(studentId);
+        //
+
         registrationService.registerStudentToCourse(foundStudent, foundCourse);
     }
 }
